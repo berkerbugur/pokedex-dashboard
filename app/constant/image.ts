0f5b3e69a76700ac63b6154cf12a61c2ebc4993d
BeBu <berker.bugur@trendyol.com>
2023-09-17 18:06:01 +0300

    add static compile images to ignore

---
+++ app/constant/image.ts
@@ -0,0 +1,47 @@
+import PokeBall1 from '@/public/pokeball-1.png'
+import PokeBall2 from '@/public/pokeball-2.png'
+import PokeBall3 from '@/public/pokeball-3.png'
+import PokeBall4 from '@/public/pokeball-4.png'
+import PokeBall5 from '@/public/pokeball-5.png'
+import PokeBall6 from '@/public/pokeball-6.png'
+import PokeBall7 from '@/public/pokeball-7.png'
+import PokeBall8 from '@/public/pokeball-8.png'
+import PokeBall9 from '@/public/pokeball-9.png'
+import PokeBall10 from '@/public/pokeball-10.png'
+
+import PokeLoad from '@/public/poke-load.png'
+import GitHub from "@/public/github.png";
+import LinkedIn from "@/public/linkedin.png";
+import Surprised from "@/public/surprised.png"
+import {StaticImageData} from "next/image";
+
+const PokeballCollection: StaticImageData[] = [
+    PokeBall1,
+    PokeBall2,
+    PokeBall3,
+    PokeBall4,
+    PokeBall5,
+    PokeBall6,
+    PokeBall7,
+    PokeBall8,
+    PokeBall9,
+    PokeBall10
+];
+
+export {
+    PokeBall1,
+    PokeBall2,
+    PokeBall3,
+    PokeBall4,
+    PokeBall5,
+    PokeBall6,
+    PokeBall7,
+    PokeBall8,
+    PokeBall9,
+    PokeBall10,
+    PokeballCollection,
+    PokeLoad,
+    GitHub,
+    LinkedIn,
+    Surprised
+}
---
+++ app/constant/url.ts
@@ -0,0 +1,7 @@
+const githubUrl = 'https://github.com/berkerbugur'
+const linkedInUrl = 'https://www.linkedin.com/in/berker-bugur'
+const pokeApiUrl = 'https://pokeapi.co/'
+const pokeDexUrl = 'https://www.pokemon.com/us/pokedex/'
+const themeUrl = 'https://www.youtube.com/watch?v=rg6CiPI6h2g&ab_channel=kerwinpogi092'
+
+export {githubUrl, linkedInUrl, pokeApiUrl, pokeDexUrl, themeUrl}
